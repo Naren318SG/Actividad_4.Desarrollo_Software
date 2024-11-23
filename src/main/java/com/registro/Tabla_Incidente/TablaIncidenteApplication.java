0ef3bb24921ad92746e3b163d25f9c230c4d8cc8
Estudiante <Estudiante@LAPTOP-JNGV5LME>
2024-11-23 18:41:55 -0500

    Updated

---
+++ src/main/java/com/registro/Tabla_Incidente/TablaIncidenteApplication.java
@@ -0,0 +1,13 @@
+package com.registro.Tabla_Incidente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablaIncidenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablaIncidenteApplication.class, args);
+	}
+
+}
